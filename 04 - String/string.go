de90406d7097dd559a5faba21ea6d63bd26c89af
mhrk04 <haziqrohaizan@gmail.com>
2022-12-11 12:56:47 +0800

    add sample code for string

---
+++ 04 - String/string.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("My Name :")
+	fmt.Println("Haziq Rohaizan")
+
+	// function for string
+
+	fmt.Println(len("Haziq Rohaizan"))
+	// output will show in byte form
+	fmt.Println("Mhaziqrk"[0])
+
+}
